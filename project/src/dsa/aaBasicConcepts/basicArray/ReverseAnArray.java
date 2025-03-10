c384f2bd6d87360772abc2aed14034e3bf0f86d7
sumeght <sumegh_tembhurne@epam.com>
2025-03-10 14:36:53 +0530

    basic array question

--- project/src/dsa/aaBasicConcepts/basicArray/ReverseAnArray.java
+++ project/src/dsa/aaBasicConcepts/basicArray/ReverseAnArray.java
@@ -15,16 +15,16 @@ public class ReverseAnArray {
         }
         return ;
     }
-}
 
-class Main {
+
     // Function to print array
-    public static void printArray(int arr[], int n) {
+    static void printArray(int arr[], int n) {
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
+
 
     public static void main(String[] args) {
         int n = 5;

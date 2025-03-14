ab4372b780ae2cc1ada0e3335e490bb70d14da54
Sumegh G. Tembhurne <84231944+Sumegh22@users.noreply.github.com>
2025-03-14 14:22:10 +0530

    Array qa (#8)
    * basic array question
    
    * basic array question
    
    * basic array question
    
    * basic hasing questions
    
    * basic hasing questions
    
    ---------
    
    Co-authored-by: sumeght <sumegh_tembhurne@epam.com>

---
+++ project/src/dsa/aaBasicConcepts/hasing/MostFrequentElement.java
@@ -0,0 +1,33 @@
+package project.src.dsa.aaBasicConcepts.hasing;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MostFrequentElement {
+    // Using streams and lib methods to solve the problem
+    public int mostFrequentElement(int[] nums) {
+        Map<Integer, Long> frequencyMap;
+        if (nums == null) {
+            return 0;
+        }
+        frequencyMap = Arrays.stream(nums)
+                .boxed()
+                .collect(Collectors.groupingBy(Integer::intValue, Collectors.counting()));
+
+        return frequencyMap.entrySet().stream().max(Map.Entry.comparingByValue()).get().getKey();
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 4, 5, 5, 6, 7};
+
+        /* Creating an instance of
+        Solution class */
+        MostFrequentElement sol = new MostFrequentElement();
+
+        /* Function call to get the second
+        highest occurring element in array */
+        int ans = sol.mostFrequentElement(nums);
+
+        System.out.println("The highest occurring element in the array is: " + ans);
+    }
+}

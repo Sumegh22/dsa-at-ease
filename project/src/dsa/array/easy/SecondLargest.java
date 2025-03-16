3165c169af1b3aee4b819db9cee6d48708d8c389
Sumegh G. Tembhurne <84231944+Sumegh22@users.noreply.github.com>
2025-03-16 13:00:36 +0530

    final (#11)
    * add SelectionSort
    
    * adding bubble sort
    
    * adding Insertion Sort and Merge Sort
    
    * adding Quick Sort and Merge Sort
    
    * updating logic to find second largest element in Array
    
    ---------
    
    Co-authored-by: sumeght <sumegh_tembhurne@epam.com>

--- project/src/dsa/array/easy/SecondLargest.java
+++ project/src/dsa/array/easy/SecondLargest.java
@@ -1,27 +1,45 @@
 package project.src.dsa.array.easy;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class SecondLargest {
     public static void main(String[] args) {
-        int[] input = {12, 35, 1, 10, 34, 1};
-        int n = input.length;
+        int[] input = {12, 39, 1, 10, 34, 131};
 
-        int mx = Integer.MIN_VALUE;
-        int sm = Integer.MIN_VALUE;
-
-        for (int i = 0; i < n; i++) {
-            if (input[i] > mx) {
-                sm = mx;
-                mx = input[i];
-            } else if (input[i] > sm && input[i] < mx) {
-                sm = input[i];
-            }
-        }
-
-        if (sm == Integer.MIN_VALUE) {
+        int secondLargest = secondLargestElementByStreams(input);
+        if (secondLargest == Integer.MIN_VALUE) {
             System.out.println("No second largest element found");
         } else {
-            System.out.println("Second Largest Element: " + sm);
+            System.out.println("Second Largest Element: " + secondLargest);
         }
     }
+
+    public static int secondLargestElementByStreams(int[] nums) {
+        return Arrays.stream(nums)
+                .boxed()  // Convert int to Integer for sorting & distinct operations
+                .distinct() // Remove duplicates
+                .sorted(Comparator.reverseOrder()) // Sort in descending order
+                .skip(1) // Skip the largest element
+                .findFirst() // Get the second largest element
+                .orElse(-1); // If no second largest exists, return -1
+    }
+
+    public static int secondLargestElement(int[] nums) {
+        int largest = nums[0];
+        int sl = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > largest) {
+                sl = largest;
+                largest = nums[i];
+            } else if (nums[i] > sl && nums[i] < largest) {
+                sl = nums[i];
+            }
+        }
+        return sl;
+
+    }
+
 }
 

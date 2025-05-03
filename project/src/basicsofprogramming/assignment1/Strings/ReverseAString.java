1c420bd40d09564351bf493ad3a35ed4f2ba0082
Sumegh G. Tembhurne <84231944+Sumegh22@users.noreply.github.com>
2025-05-03 16:40:31 +0530

    adding ReverseAString.java

---
+++ project/src/basicsofprogramming/assignment1/Strings/ReverseAString.java
@@ -0,0 +1,40 @@
+package basicsofprogramming.assignment1.Strings;
+
+public class ReverseAString {
+
+    public static String reverse1(String str){
+      StringBuilder sb = new StringBuilder(str)
+        return sb.reverse().toString();
+    }
+
+      public static String reverse2(String s){
+        int left = 0;
+        int right = s.length()-1;
+
+        while(left<right) {
+          char ch = s.charAt(left);
+          s.replace(s.charAt(left), s.charAt(right));
+          s.replace(s.charAt(right), c);
+          left++; right--;
+        }          
+        return s;
+    }
+
+      public static String reverseByRecursion(String str){
+        if(str.length()<=1 || str == null) return str;
+        
+        return reverseByRecursion(str.substring(1))+str.charAt(0);
+    }
+
+    /**
+
+    The mentioned code can be used to reverse any given String, Three possible apporaches are mentioned in this class
+    
+    */
+  
+    public static void main(String[] args) {
+        System.out.println(reverseByRecursion("jamaica"));
+
+    }
+
+}
